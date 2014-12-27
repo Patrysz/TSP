a53ca89daa56b77833480d39e84756d7e9e1611d
bartlomiejnisztuk <bnisztuk@gmail.com>
2014-12-27 19:03:09 +0100

    initial commit

---
+++ Presentation.TSP/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace Presentation.TSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadCsvToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelCities = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.labelDistance = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.programToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(593, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadCsvToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadCsvToolStripMenuItem
+            // 
+            this.loadCsvToolStripMenuItem.Name = "loadCsvToolStripMenuItem";
+            this.loadCsvToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.loadCsvToolStripMenuItem.Text = "Load data";
+            this.loadCsvToolStripMenuItem.Click += new System.EventHandler(this.loadCsvToolStripMenuItem_Click);
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 328);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(569, 83);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // labelCities
+            // 
+            this.labelCities.AutoSize = true;
+            this.labelCities.Location = new System.Drawing.Point(371, 83);
+            this.labelCities.Name = "labelCities";
+            this.labelCities.Size = new System.Drawing.Size(81, 13);
+            this.labelCities.TabIndex = 2;
+            this.labelCities.Text = "Num. of palces:";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(12, 80);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(353, 211);
+            this.logTextBox.TabIndex = 3;
+            this.logTextBox.Text = "";
+            // 
+            // labelDistance
+            // 
+            this.labelDistance.AutoSize = true;
+            this.labelDistance.Location = new System.Drawing.Point(371, 104);
+            this.labelDistance.Name = "labelDistance";
+            this.labelDistance.Size = new System.Drawing.Size(92, 13);
+            this.labelDistance.TabIndex = 4;
+            this.labelDistance.Text = "Shortest distance:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Calculating time:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Status";
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(56, 28);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(76, 13);
+            this.labelStatus.TabIndex = 7;
+            this.labelStatus.Text = "File not loaded";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(15, 51);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(75, 23);
+            this.buttonRun.TabIndex = 8;
+            this.buttonRun.Text = "Run";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(290, 297);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 9;
+            this.buttonClear.Text = "Clear log";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 423);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelDistance);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.labelCities);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadCsvToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelCities;
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.Label labelDistance;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ Presentation.TSP/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using AntColony;
+using Presentation.TSP.Properties;
+
+namespace Presentation.TSP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static Random random = new Random(0);
+
+        static int alpha = 3; // influence of pheromone on direction
+        static int beta = 2;  // influence of adjacent node distance
+
+        static double rho = 0.01; // pheromone decrease factor
+        static double Q = 2.0;   // pheromone increase factor
+        private List<List<int>> _dataFromFile;
+
+        public List<List<int>> DataFromFile
+        {
+            get { return _dataFromFile??(_dataFromFile=new List<List<int>>()); }
+            set { _dataFromFile = value; }
+        }
+
+        private void loadCsvToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataFromFile.Clear();
+            var f = new OpenFileDialog();
+            if (f.ShowDialog()==DialogResult.OK)
+            {
+                var file = f.FileName;
+                var reader = new StreamReader(File.OpenRead(@file));
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        try
+                        {
+                            var values = line.Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+                            DataFromFile.Add(values);
+                        }
+                        catch (InvalidCastException ex)
+                        {                            
+                            throw new InvalidCastException(String.Format(Resources.Form1_loadCsvToolStripMenuItem_Click_Invalid_file_format_Line___0_,line));
+                        }                                                
+                    }
+                }
+            }
+
+            labelStatus.Text = Resources.Form1_loadCsvToolStripMenuItem_Click_Data_loaded_succesfully;
+        }
+
+        private void buttonRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numCities = 10;
+                int numAnts = 4;
+                int maxTime = 1000;
+
+
+                logTextBox.Text += String.Format("Number of cities: {0} \n", numCities);
+                logTextBox.Text += String.Format("Number of ants: {0} \n", numAnts);                
+                logTextBox.Text += String.Format("Maximum time: {0} \n", maxTime);                              
+                logTextBox.Text += String.Format("Alpha (pheromone influence): {0} \n", alpha);                              
+                logTextBox.Text += String.Format("Beta (local node influence): {0} \n", beta);                              
+                logTextBox.Text += String.Format("Rho (pheromone evaporation coefficient): {0} \n", rho.ToString("F2"));        
+                logTextBox.Text += String.Format("Q (pheromone deposit factor): {0} \n", Q.ToString("F2"));        
+                logTextBox.Text += String.Format("Initialing graph distances \n");
+
+                //int[][] dists = AntColonyProgram.MakeGraphDistancesDemo(numCities); //generate random distances
+                if (DataFromFile == null || DataFromFile.Count == 0)
+                {
+                    logTextBox.Text += "Data from file not loaded \n";
+                    logTextBox.Text += "Operation cancelled \n";
+                    return;
+                }
+                    
+
+                int[][] dists = AntColonyProgram.MakeGraphDistances(numCities,DataFromFile);
+
+
+                logTextBox.Text += String.Format("\n Initialing ants to random trails \n");
+ 
+                int[][] ants = AntColonyProgram.InitAnts(numAnts, numCities); // initialize ants to random trails
+               // ShowAnts(ants, dists);
+
+                int[] bestTrail =AntColonyProgram.BestTrail(ants, dists); // determine the best initial trail
+                double bestLength =AntColonyProgram.Length(bestTrail, dists); // the length of the best trail
+
+                logTextBox.Text+=("\nBest initial trail length: " + bestLength.ToString("F1") + "\n");
+               logTextBox.Text+= AntColonyProgram.Display(bestTrail);
+
+                logTextBox.Text += ("\nInitializing pheromones on trails");
+                double[][] pheromones =AntColonyProgram.InitPheromones(numCities);
+
+                int time = 0;
+                logTextBox.Text += ("\nEntering UpdateAnts - UpdatePheromones loop\n");
+                while (time < maxTime)
+                {
+                  AntColonyProgram.UpdateAnts(ants, pheromones, dists);
+                  AntColonyProgram.UpdatePheromones(pheromones, ants, dists);
+
+                    int[] currBestTrail =AntColonyProgram.BestTrail(ants, dists);
+                    double currBestLength =AntColonyProgram.Length(currBestTrail, dists);
+                    if (currBestLength < bestLength)
+                    {
+                        bestLength = currBestLength;
+                        bestTrail = currBestTrail;
+                        logTextBox.Text += ("\n New best length of " + bestLength.ToString("F1") + " found at time " + time);
+                    }
+                    ++time;
+                }
+
+                logTextBox.Text += ("\nTime complete");
+
+                logTextBox.Text += ("\nBest trail found:");
+                logTextBox.Text+= AntColonyProgram.Display(bestTrail);
+                logTextBox.Text += ("\nLength of best trail found: " + bestLength.ToString("F1"));
+
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            logTextBox.Clear();
+        }
+    }
+}
